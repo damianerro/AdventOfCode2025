5f7210e79e2eff8d3e6980d504543e93c52e711c
damianerro <damianec@gmail.com>
2025-12-01 16:17:40 -0700

    working on day1_2

---
+++ day1_2.py
@@ -0,0 +1,44 @@
+import re
+dial = 50
+zero_count= 0
+input_list = []
+zero_clicks= 0
+
+with open("input-day1-1.txt", "r") as file:
+    for line in file.readlines():
+        digits = int(re.findall("\d+", line)[0])
+        if "L" in line:
+            input_list.append((-1*digits))
+        else:
+            input_list.append(digits)   
+# print(input_list)
+
+for i in input_list:  
+    if i < 0:
+        zero_clicks += int(i/-100)
+        dial += (i%(-100))
+        
+        if dial < 0:
+            dial+=100
+            zero_clicks+=1 
+        else:
+            dial = dial%-100
+            
+        if dial == 0:
+            zero_count += 1
+    else:
+        zero_clicks += int(i/100)
+        
+        dial = dial + (i%100)
+        
+        if dial > 99:
+            zero_clicks += 1
+            dial = dial % 100
+            # print(dial)
+        if dial == 0:
+            zero_count += 1
+    # print(dial)
+
+
+print(zero_count)
+print(zero_clicks)
